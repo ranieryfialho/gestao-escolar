50fbdb11f83eda465f1f99df1c6c924370ef4537
ranieryfialho <ranieryfialho@gmail.com>
2025-09-30 16:46:54 -0300

    rollback das funções

--- src/firebase.js
+++ src/firebase.js
@@ -14,12 +14,9 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Inicializa a aplicação Firebase
 const app = initializeApp(firebaseConfig);
 
-// Criamos e exportamos os serviços que os outros ficheiros vão usar.
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-// Especificamos a região para garantir uma conexão estável com as funções
 export const functions = getFunctions(app, 'us-central1');
 export const storage = getStorage(app);
